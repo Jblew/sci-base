b3f954a6ba8c0cd657e0bb1b34d154588f105adb
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2021-10-31 11:57:52 +0100

    feat: zotero reference and bibliography fetching

--- fetch-zotero/index.js
+++ fetch-zotero/index.js
@@ -1,4 +1,9 @@
-const { getFromZotero } = require("./utils");
+const { getFromZotero, writeToFile } = require("./utils");
+
+const BIBTEX_PATH = process.env.BIBTEX_PATH;
+if (!BIBTEX_PATH) throw new Error("Missing env BIBTEX_PATH");
+const ZOTEROJSON_PATH = process.env.ZOTEROJSON_PATH;
+if (!ZOTEROJSON_PATH) throw new Error("Missing env ZOTEROJSON_PATH");
 
 (async (startFn) => {
   try {
@@ -12,7 +17,10 @@ const { getFromZotero } = require("./utils");
 async function fetchFromZotero() {
   const items = await getFromZotero("items", { format: "json" });
   console.log(items);
+  await writeToFile(ZOTEROJSON_PATH, JSON.stringify(items, undefined, 2));
 
   const bibtex = await getFromZotero("items", { format: "bibtex" });
   console.log(bibtex);
+  await writeToFile(BIBTEX_PATH, bibtex);
+  console.log("Done");
 }
--- fetch-zotero/utils.js
+++ fetch-zotero/utils.js
@@ -1,19 +1,14 @@
 const Axios = require("axios");
+const fs = require("fs");
 
 const ZOTERO_APIKEY = process.env.ZOTERO_APIKEY;
 if (!ZOTERO_APIKEY) throw new Error("Missing env ZOTERO_APIKEY");
 const ZOTERO_USERID = process.env.ZOTERO_USERID;
 if (!ZOTERO_USERID) throw new Error("Missing env ZOTERO_USERID");
 
-const encodeGetParams = (p) =>
-  Object.entries(p)
-    .map((kv) => kv.map(encodeURIComponent).join("="))
-    .join("&");
-
 async function getFromZotero(apiUrl, params) {
   const apikey = ZOTERO_APIKEY;
   const userid = ZOTERO_USERID;
-  const getParams = encodeGetParams(params);
   const url = `https://api.zotero.org/users/${userid}/${apiUrl}`;
   const resp = await Axios({
     method: "get",
@@ -30,4 +25,8 @@ async function getFromZotero(apiUrl, params) {
   return resp.data;
 }
 
-module.exports = { encodeGetParams, getFromZotero };
+async function writeToFile(path, data) {
+  return fs.writeFileSync(path, data);
+}
+
+module.exports = { getFromZotero, writeToFile };
